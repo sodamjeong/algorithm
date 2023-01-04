32002fcea641f36ea96ddc053566a521a2ad3272
sodamjeong <sodami828@gmail.com>
2023-01-04 23:20:20 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,11 @@
+x = list(map(int,input().split()))
+y = [1,1,2,2,2,8]
+
+a = y[0] - x[0]
+b = y[1] - x[1]
+c = y[2] - x[2]
+d = y[3] - x[3]
+e = y[4] - x[4]
+f = y[5] - x[5]
+
+print(a, b, c, d, e, f)

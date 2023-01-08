fe035bbe886cf9c3bcb7d63b939cd644883ae190
sodamjeong <sodami828@gmail.com>
2023-01-08 21:25:05 +0900

    [Bronze II] Title: 나머지, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,15 +1,8 @@
 n = []
-m = {}
 
 for i in range(10):
     a = int(input())
     b = a % 42
     n.append(b)
 
-for num in n:
-    if num not in m:
-        m[num] = 1
-    else:
-        m[num] += 1
-
-print(len(m))
+print(len(set(n)))

3a0fa740138b05bc7e90303d92922875b8e68ac7
sodamjeong <sodami828@gmail.com>
2023-01-18 11:15:41 +0900

    [Bronze II] Title: 문자열 반복, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,13 +1,7 @@
-t = int(input())
-x = []
-y = []
-
-for i in range(t):
-    a,b = input().split()
-    for n in b:
-        x.append(n)
-    for m in x:
-        y.append(m*int(a))
-    print(''.join((y)))
-    x.clear()
-    y.clear()
+n = []
+for i in range(int(input())):
+    a,b = input(). split()
+    for x in b:
+        n.append(x*int(a))
+    print(''.join(n))
+    n = []

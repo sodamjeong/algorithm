7617896343b7edb3b3c6815722a94bebb94b113e
sodamjeong <sodami828@gmail.com>
2023-01-20 17:08:07 +0900

    Update ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -1,22 +1,12 @@
-t = int(input())
-n ={}
-value = []
-key = []
-
-for i in range(1,t+1):
-    tt = int(input())
+for i in range(int(input())):
+    t = int(input())
     m = list(map(int,input().split()))
+    n ={}
+     
     for a in m:
         if a not in n:
             n[a] = 1
         else :
             n[a] += 1
-    for v in n.values():
-        value.append(v)
-    for k in n:
-        if n[k] == max(value):
-            key.append(k)
-    print(f'#{tt}', max(key))
-    n = {}
-    value = []
-    key = []
+ 
+    print(f'#{t}', max([k for k in n if n[k] == max([v for v in n.values()])]))

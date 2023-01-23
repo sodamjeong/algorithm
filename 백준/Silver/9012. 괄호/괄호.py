dfb350ab6d2526c3ad480c6f6c1a95682a8652fe
sodamjeong <sodami828@gmail.com>
2023-01-24 02:21:23 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,15 @@
+for i in range(int(input())):
+    n = input()
+    cnt = 0
+    for x in n:
+        if x == '(':
+            cnt += 1
+        elif x == ')':
+            cnt += -1
+            if cnt < 0:
+                print('NO')
+                break
+    if cnt == 0:
+        print('YES')
+    elif cnt > 0:
+        print('NO')

b5293fcae9ac27ecfbca4f12de59951bfc9ec09c
sodamjeong <sodami828@gmail.com>
2023-01-27 15:10:28 +0900

    [Silver V] Title: 단어 정렬, Time: 892 ms, Memory: 35168 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,7 +1,5 @@
 n = sorted(set([input() for _ in range(int(input()))]))
-m = []
 for x in range(51):
     for y in n:
         if len(y) == x:
-            m.append(y)
-print(*(m),sep='\n')
+            print(y)

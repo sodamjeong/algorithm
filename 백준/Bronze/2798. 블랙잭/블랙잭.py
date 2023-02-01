64c184628f378ed3f6ba5b7c5927be88942f574b
sodamjeong <sodami828@gmail.com>
2023-02-01 13:36:06 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 37628 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,10 +1,10 @@
 n,m = map(int,input().split())
 card = list(map(int,input().split()))
-ans = []
-
+c = []
 for x in range(0, n-2):
     for y in range(x+1, n-1):
         for z in range(y+1,n):
-            if card[x]+card[y]+card[z] <= m:
-                ans.append(card[x]+card[y]+card[z])
-print(max(ans))
+            t = card[x]+card[y]+card[z]
+            if t <= m:
+                c.append(t)
+print(max(c))

d60c8f720c29fc2f835db399e2421476c5925f65
sodamjeong <sodami828@gmail.com>
2023-02-11 20:55:20 +0900

    [Silver III] Title: 피보나치 함수, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,9 +1,6 @@
 for _ in range(int(input())):
     x,y = 1,0
     n = int(input())
-    if n == 0:
-        print(x, y)
-    else:
-        for _ in range(n):
-            x,y = y,x+y
-        print(x,y)
+    for _ in range(n):
+        x,y = y,x+y
+    print(x,y)

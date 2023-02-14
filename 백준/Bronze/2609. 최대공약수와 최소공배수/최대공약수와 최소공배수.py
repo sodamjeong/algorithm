7e80dd1e28ff749025c59c7902aafdd57c32c26d
sodamjeong <sodami828@gmail.com>
2023-02-14 16:05:11 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,8 +1,7 @@
 a,b = map(int,input().split())
 n = []
-for x in range(1,a+b):
-    if a % x == 0 and b % x == 0:
-        n.append(x)
-m = max(n)        
-print(m)
-print(m*(a//m)*(b//m))
+for i in range(1,a+b):
+    if (a%i,b%i) == (0,0):
+        n.append(i)       
+print(max(n))
+print(a*b//max(n))

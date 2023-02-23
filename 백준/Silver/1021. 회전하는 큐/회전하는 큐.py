bc6bc4eda09fc84f74a87647efb30944cd61b3a4
sodamjeong <sodami828@gmail.com>
2023-02-23 17:50:15 +0900

    [Silver III] Title: 회전하는 큐, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,24 +1,10 @@
-from collections import deque
 n,m = map(int,input().split())
-lst = deque(range(1,n+1))
-lst2 = deque(range(1,n+1))
+lst = list(range(1,n+1))
 num = list(map(int,input().split()))
-answer = 0
+cnt = 0
 
 for i in num:
-    cnt = 0
-    cnt2 = 0
-    while 1:
-        if lst[0] == i:
-            lst.popleft()
-            break
-        lst.rotate(1)
-        cnt += 1
-    while 1:
-        if lst2[0] == i:
-            lst2.popleft()
-            break
-        lst2.rotate(-1)
-        cnt2 += 1
-    answer += min(cnt,cnt2)
-print(answer)
+    x = lst.index(i)
+    cnt += min(x,len(lst)-x)
+    lst = lst[x+1:] + lst[:x]
+print(cnt)

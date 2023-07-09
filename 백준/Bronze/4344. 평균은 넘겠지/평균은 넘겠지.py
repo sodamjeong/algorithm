45d447cc7bb27f5e61ee47e8ba3d66b64598e565
sodamjeong <sodami828@gmail.com>
2023-07-09 22:29:00 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,11 +1,12 @@
 C = int(input())
 
 for i in range(C):
-    score = list(map(int,input().split()))
-    n = sum(score[1:])/score[0]
-    m = 0
-    for a in score[1:]:
-        if a > n :
-            m += 1
-    percent = m / score[0] * 100
-    print(f'{percent:.3f}%')
+    N = list(map(int, input().split()))
+    M = sum(N[1:])/N[0]
+    cnt = 0
+    for score in N[1:]:
+        if score > M:
+            cnt += 1
+    B = cnt/N[0] * 100 
+    print(f'{B:.3f}%')
+

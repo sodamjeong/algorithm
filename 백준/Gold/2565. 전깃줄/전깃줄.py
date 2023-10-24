858bd82b7a49f7a7c674b950ea3e0f600ed98837
sodamjeong <sodami828@gmail.com>
2023-10-24 11:15:46 +0900

    [Gold V] Title: 전깃줄, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,13 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-n=int(input())
-ans=[1]*n
-lst=sorted([list(map(int,input().split())) for _ in range(n)])
+n = int(input())
+ans = [1]*n
+line = sorted([list(map(int,input().split())) for _ in range(n)])
 
-for i in range(1,n):
-    for j in range(0,i):
-        if lst[i][0]>lst[j][0] and lst[i][1]>lst[j][1]:
-            ans[i]=max(ans[i],ans[j]+1)
+for i in range(n):
+    for j in range(i):
+        if line[i][1]>line[j][1]:
+            ans[i] = max(ans[i],ans[j]+1)
 
-print(n-max(ans))
+print(n - max(ans))
